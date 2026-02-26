3bab4cde7468af73a31636cd154152942bdadfaf
NguyenThiNTram <ngoctram31204@gmail.com>
2026-02-26 22:06:06 +0700

    Fix Swagger UI mặc định

--- BoxHub.API/Program.cs
+++ BoxHub.API/Program.cs
@@ -135,10 +135,18 @@ namespace BoxHub.API
             //    app.UseSwaggerUI();
             //}
 
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseForwardedHeaders();
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BoxHub API V1");
+                c.RoutePrefix = "swagger";
+            });
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
